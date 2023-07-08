d21e880b9b3cb475dae9ba4bd10e4176b94d5b0b
TanyaMosk <tanya856227@gmail.com>
2023-07-08 22:16:30 +0200

    Fixes - 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import './common.css';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchSearchesImages } from './pixabay-api';
 import { renderMarkupImage } from './markup';
+
+
+
 
 // import NewSearchesImages from './pixabay-api'
 const refs = {
@@ -14,44 +20,123 @@ refs.loadMoreBtn.addEventListener('click', onLoadMoreBtn);
 refs.loadMoreBtn.classList.add('is-hidden');
 let searchQuery = '';
 let page = 1;
+const pages = 40;
 
 function onSearchForm(evt) {
   evt.preventDefault();
   
-
   searchQuery = evt.currentTarget.searchQuery.value.trim();
 
   page = 1;
   console.log(searchQuery);
-  if (searchQuery === '') {
-    refs.gallery.innerHTML = '';
-    return alert("Sorry, there are no images matching your search query. Please try again.");
-}
-  fetchSearchesImages(searchQuery, page).then((data) => {
-    // const hits = data.hits;
-    console.log(data.hits);
-    if (data.hits.length === 0) {
-      refs.gallery.innerHTML = '';
-      refs.loadMoreBtn.classList.add('is-hidden');
-      console.log(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    } else {
-      refs.gallery.innerHTML = '';
-      refs.loadMoreBtn.classList.remove('is-hidden');
-      const markup = data.hits.map(renderMarkupImage).join('');
-      // console.log(markup);
-      refs.gallery.insertAdjacentHTML('beforeend', markup);
-      console.log(`Hooray! We found ${data.totalHits} images.`);
-    }
-  });
+  // if (searchQuery === '') {
+  //   refs.gallery.innerHTML = '';
+  //   Notiflix.Notify.failure(
+  //     'Sorry, there are no images matching your search query. Please try again.',
+  //     {
+  //       position: 'center-bottom',
+  //       timeout: 2000,
+  //       width: '520px',
+  //       fontSize: '20px',
+  //       cssAnimationStyle: 'from-bottom',
+  //     });
+  //   return
+  // }
+  fetchSearchesImages(searchQuery, page)
+    .then(data => {
+      if (data.totalHits === 0 || searchQuery === '') {
+        refs.gallery.innerHTML = '';
+        refs.loadMoreBtn.classList.add('is-hidden');
+
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.',
+          {
+            position: 'center-bottom',
+            timeout: 2000,
+            width: '520px',
+            fontSize: '20px',
+            cssAnimationStyle: 'from-bottom',
+          }
+        );
+        return
+      } else {
+        refs.gallery.innerHTML = '';
+        refs.loadMoreBtn.classList.remove('is-hidden');
+        const markup = data.hits.map(renderMarkupImage).join('');
+        refs.gallery.insertAdjacentHTML('beforeend', markup);
+
+        let lightbox = new SimpleLightbox('.gallery-img a', {
+          captionsData: 'alt',
+          captionDelay: '250',
+        });
+
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`, {
+          timeout: 2000,
+          position: 'center-bottom',
+          width: '460px',
+          fontSize: '18px',
+          cssAnimationStyle: 'from-bottom',
+        });
+        // console.log(`Hooray! We found ${data.totalHits} images.`);
+      }
+      // console.log(data.hits.length);
+
+      if (data.hits.length < pages) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results.",
+          {
+            timeout: 2000,
+            position: 'center-bottom',
+            width: '520px',
+            fontSize: '18px',
+            cssAnimationStyle: 'from-bottom',
+          }
+        );
+      }
+    })
+    .catch(onError);
 }
 
 function onLoadMoreBtn() {
   page += 1;
-  fetchSearchesImages(searchQuery, page).then(({ hits }) => {
-    console.log(hits);
-    const markup = hits.map(renderMarkupImage).join('');
-    refs.gallery.insertAdjacentHTML('beforeend', markup);
-  });
+  fetchSearchesImages(searchQuery, page)
+    .then(data => {
+      if (data.hits.length < pages) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        console.log(data.hits);
+        // const markup = data.hits.map(renderMarkupImage).join('');
+        
+
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results.",
+          {
+            timeout: 2000,
+            position: 'center-bottom',
+            width: '520px',
+            fontSize: '18px',
+            cssAnimationStyle: 'from-bottom',
+          }
+        );
+      } else {
+        refs.gallery.insertAdjacentHTML(
+          'beforeend',
+          data.hits.map(renderMarkupImage).join('')
+        );
+      }
+    })
+    .catch(onError);
 }
+
+function onError() {
+    Notiflix.Notify.failure(
+      'Oops, something went wrong 🥺. Please try reloading the page!',
+      {
+        timeout: 2000,
+        position: 'center-bottom',
+        width: '460px',
+        fontSize: '18px',
+        cssAnimationStyle: 'from-bottom',
+      }
+    );
+};
--- src/markup.js
+++ src/markup.js
@@ -1,3 +1,4 @@
+
 export function renderMarkupImage({
   webformatURL,
   largeImageURL,
@@ -9,22 +10,25 @@ export function renderMarkupImage({
 }) {
   return `
   <div class="photo-card">
-  <a href="${largeImageURL}">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-      </a>
+    <div class="gallery-img">
+      <a href="${largeImageURL}" >
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" width="240" />
+        </a>
+        </div class="desc">
       <div class="info">
         <p class="info-item">
-        <b>Likes${likes}</b>
+        <b>Likes ${likes}</b>
           </p>
         <p class="info-item">
-        <b>Views${views}</b>
+        <b>Views ${views}</b>
           </p>
         <p class="info-item">
-        <b>Comments${comments}</b>
+        <b>Comments ${comments}</b>
           </p>
         <p class="info-item">
-        <b>Downloads${downloads}</b>
+        <b>Downloads ${downloads}</b>
           </p>
       </div>
   </div> `;
 }
+
